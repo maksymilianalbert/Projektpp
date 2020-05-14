5954b1094e4472f94c1c389d1636d7e69f95fc1b
maksymilianalbert <64994449+maksymilianalbert@users.noreply.github.com>
2020-05-14 23:41:09 +0200

    Add files via upload

---
+++ enkrypcja.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	char wiad[100], zn;
+	int a, klucz;
+
+	cout << "Wprowadz slowo do enkrypcji: ";
+	cin.getline(wiad, 100);
+	cout << "Podaj klucz szyfrowania: ";
+	cin >> klucz;
+
+	for (a = 0; wiad[a] != '\0'; ++a) {
+		zn = wiad[a];
+
+		if (zn >= 'a' && zn <= 'z') {
+			zn = zn + klucz;
+
+			if (zn > 'z') {
+				zn = zn - 'z' + 'a' - 1;
+			}
+
+			wiad[a] = zn;
+		}
+		else if (zn >= 'A' && zn <= 'Z') {
+			zn = zn + klucz;
+
+			if (zn > 'Z') {
+				zn = zn - 'Z' + 'A' - 1;
+			}
+
+			wiad[a] = zn;
+		}
+	}
+
+	cout << "Zaszyfrowana wiadmoœæ: " << wiad;
+
+	return 0;
+}
