466924e0c5f04603e07de25e32547c0f62a56df1
maksymilianalbert <64994449+maksymilianalbert@users.noreply.github.com>
2020-06-07 17:41:24 +0200

    Update enkrypcja_i_dekrypcja.cpp

--- enkrypcja_i_dekrypcja.cpp
+++ enkrypcja_i_dekrypcja.cpp
@@ -1,92 +1,69 @@
-#include<iostream>
-
+#include <iostream>
+#include "Szyfr.h"
 using namespace std;
 
-int main()
+void Szyfr::szyfruj(char wiad[100],char zn, int a, int klucz) {
+	cout << "Wprowadz slowo do enkrypcji: ";
+	cin.getline(wiad, 100);
+	cout << "Podaj klucz szyfrowania: ";
+	cin >> klucz;
 
+	for (a = 0; wiad[a] != '\0'; ++a) {
+		zn = wiad[a];
 
+		if (zn >= 'a' && zn <= 'z') {
+			zn = zn + klucz;
 
-
-{
-	char wiad[100], zn;
-	int a, klucz, b;
-
-	cout << " jeżeli chcesz dokonac enkrypcji wpisz 1" << endl;
-	cout << "jezeli chcesz dokonac dekrypcji wpisz 0" << endl;
-	cin >> b;
-
-	if (b == 1)
-	{
-
-		cout << "Wprowadz slowo do enkrypcji: ";
-		cin.getline(wiad, 100);
-		cout << "Podaj klucz szyfrowania: ";
-		cin >> klucz;
-
-		for (a = 0; wiad[a] != '\0'; ++a) {
-			zn = wiad[a];
-
-			if (zn >= 'a' && zn <= 'z') {
-				zn = zn + klucz;
-
-				if (zn > 'z') {
-					zn = zn - 'z' + 'a' - 1;
-				}
-
-				wiad[a] = zn;
+			if (zn > 'z') {
+				zn = zn - 'z' + 'a' - 1;
 			}
-			else if (zn >= 'A' && zn <= 'Z') {
-				zn = zn + klucz;
 
-				if (zn > 'Z') {
-					zn = zn - 'Z' + 'A' - 1;
-				}
-
-				wiad[a] = zn;
-			}
+			wiad[a] = zn;
 		}
+		else if (zn >= 'A' && zn <= 'Z') {
+			zn = zn + klucz;
 
-		cout << "Rozszyfrowana wiadmosc: " << wiad;
-
-		return 0;
-
-
-
-	}
-	else(b == 0) {
-
-		cout << "Wprowadz tekst do dekyrpcji: ";
-		cin.getline(wiad, 100);
-		cout << "Podaj klucz: ";
-		cin >> klucz;
-
-		for (a = 0; wiad[a] != '\0'; ++a) {
-			zn = wiad[a];
-
-			if (zn >= 'a' && zn <= 'z') {
-				zn = zn - klucz;
-
-				if (zn < 'a') {
-					zn = zn + 'z' - 'a' + 1;
-				}
-
-				wiad[a] = zn;
+			if (zn > 'Z') {
+				zn = zn - 'Z' + 'A' - 1;
 			}
-			else if (zn >= 'A' && zn <= 'Z') {
-				zn = zn - klucz;
 
-				if (zn > 'a') {
-					zn = zn + 'Z' - 'A' + 1;
-				}
-
-				wiad[a] = zn;
-			}
+			wiad[a] = zn;
 		}
-
-		cout << "Zaszyfrowana wiadomosc: " << wiad;
-
-		return 0;
 	}
+
+	cout << "Rozszyfrowana wiadmosc: " << wiad;
+}
+void Szyfr::deszyfruj(char wiad[100], int klucz, char zn, int a) {
+	cout << "Wprowadz tekst do dekyrpcji: ";
+	cin.getline(wiad, 100);
+	cout << "Podaj klucz: ";
+	cin >> klucz;
+
+	for (a = 0; wiad[a] != '\0'; ++a) {
+		zn = wiad[a];
+
+		if (zn >= 'a' && zn <= 'z') {
+			zn = zn - klucz;
+
+			if (zn < 'a') {
+				zn = zn + 'z' - 'a' + 1;
+			}
+
+			wiad[a] = zn;
+		}
+		else if (zn >= 'A' && zn <= 'Z') {
+			zn = zn - klucz;
+
+			if (zn > 'a') {
+				zn = zn + 'Z' - 'A' + 1;
+			}
+
+			wiad[a] = zn;
+		}
+	}
+
+	cout << "Zaszyfrowana wiadomosc: " << wiad;
 
 }
+
 
